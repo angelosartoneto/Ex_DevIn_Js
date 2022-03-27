abb9a141ce9b4f65f11895ce59978d2f5c9f4a42
angelosartoneto <angelosartoneto@outlook.com>
2022-03-27 12:04:15 -0300

    EX03

---
+++ M1S04/EX03/parimpar.js
@@ -0,0 +1,12 @@
+//Dado um número verifique se ele é par, caso seja exibe a mensagem de que ele é par, 
+//caso não exibe a mensagem de que ele é impar. Para exibir a mensagem pode ser usado console.log
+//dica: operador ternário pode ser uma boa_
+
+function myFunction()
+{
+
+let numero = prompt("Digite um numero: ")
+
+let Num = numero % 2 == 0 ? 'Par' : 'Impar'
+window.alert(Num)
+}
