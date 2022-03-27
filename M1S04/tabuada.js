d5ec441b8a20c51e1943e9c0c82661d6270ad556
angelosartoneto <angelosartoneto@outlook.com>
2022-03-27 12:34:10 -0300

    EX04

---
+++ M1S04/tabuada.js
@@ -0,0 +1,14 @@
+//Utilizando estrutura de repetição realize a tabuada do número 2 e exibe ela no console.log da seguinte forma:
+//2 x 0 = 0
+//2 x 1 = 2
+//....
+//2 x 10 = 20
+//dica: de uma olhadinha no for._
+
+function myFunction()
+{
+
+    for(let count=1; count<=10 ; count++)
+    document.write("2 x "+count+" = " + (2*count) + "<br />");
+ }
+
